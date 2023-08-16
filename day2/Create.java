8ca135ae083ed23ca182e2090e3d5071b396c5da
Ahalya17 <141846677+Ahalya17@users.noreply.github.com>
2023-08-16 20:00:04 +0530

    Add files via upload
    day2

---
+++ day2/Create.java
@@ -0,0 +1,51 @@
+package week2.day2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Create {
+
+	public static void main(String[] args) {
+
+		
+		ChromeDriver driver=new ChromeDriver(); 
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		
+        driver.manage().window().maximize();	
+        
+        driver.findElement(By.id("username")).sendKeys("demosalesmanager");
+        
+        driver.findElement(By.id("password")).sendKeys("crmsfa");
+        
+        driver.findElement(By.className("decorativeSubmit")).click();
+        
+        String title = driver.getTitle();
+
+        System.out.println(title);
+        
+        driver.findElement(By.linkText("CRM/SFA")).click();
+
+        driver.findElement(By.linkText("Leads")).click();
+
+        driver.findElement(By.linkText("Create Lead")).click();
+        
+        driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Testleaf");
+
+        driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Ahalya");
+
+        driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Rajendran");
+        
+        driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("ahalyar17@gmail.com");
+        
+    	driver.findElement(By.id("createLeadForm_primaryPhoneNumber")).sendKeys("9342406721");
+        
+        
+        driver.findElement(By.name("submitButton")).click();
+        
+        String text = driver.findElement(By.id("viewLead_companyName_sp")).getText();
+
+        System.out.println(text);
+        
+	}
+
+}
